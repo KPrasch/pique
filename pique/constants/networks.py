190d7f8307ecfccdb797cf10cb7e2995892c2a60
derekpierre <derek.pierre@gmail.com>
2025-05-23 16:18:03 -0400

    Add some relatively common networks including base-sepolia.

--- pique/constants/networks.py
+++ pique/constants/networks.py
@@ -1,8 +1,12 @@
 NETWORKS = {
     1: {"name": "mainnet", "explorer": "etherscan.io"},
     5: {"name": "goerli", "explorer": "goerli.etherscan.io"},
-    11155111: {"name": "sepolia", "explorer": "sepolia.etherscan.io"},
+    10: {"name": "optimism-mainnet", "explorer": "optimistic.etherscan.io"},
+    42161: {"name": "arbitrum-mainnet", "explorer": "arbiscan.io"},
     137: {"name": "polygon-mainnet", "explorer": "polygonscan.com"},
+    8453: {"name": "base-mainnet", "explorer": "basescan.org"},
     80001: {"name": "polygon-mumbai", "explorer": "mumbai.polygonscan.com"},
     80002: {"name": "polygon-amoy", "explorer": "amoy.polygonscan.com"},
+    84532: {"name": "base-sepolia", "explorer": "sepolia.basescan.org"},
+    11155111: {"name": "sepolia", "explorer": "sepolia.etherscan.io"},
 }
